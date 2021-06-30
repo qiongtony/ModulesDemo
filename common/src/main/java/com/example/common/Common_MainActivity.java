e19917c250d7af78c74673cbc51d5be87e9d9442
wuweishan1995 <wws@gzqianyin.cn>
2021-06-30 18:12:26 +0800

    实现组件化和模块化的切换

---
+++ common/src/main/java/com/example/common/Common_MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Common_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.common_activity_main);
+    }
+}
