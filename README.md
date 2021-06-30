5e445deffefba8ca78bd373d9696a005e36c655f
wuweishan1995 <wws@gzqianyin.cn>
2021-06-30 15:48:41 +0800

    module统一配置

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# ModulesDemo
+# 组件化Demo
+
+实现记录：
+
+2021/6/30实现gradle语法，根据config.gradle配置对module进行统一配置
+
---
+++ app/src/main/java/com/example/modulesdemo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.modulesdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 获取buildConfig类的属性
+        String type = BuildConfig.host;
+    }
+}
